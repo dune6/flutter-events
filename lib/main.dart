954f12ac334b03ea229e1bdae5855c0b57e7fb7c
user <qwerty_zxcvbn@internet.ru>
2022-06-28 23:56:44 +0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_events/app.dart';
+
+void main() {
+  runApp(const App());
+}
---
+++ lib/ui/auth_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AuthWidget extends StatelessWidget {
+  const AuthWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Авторизация'),
+      ),
+    );
+  }
+}
